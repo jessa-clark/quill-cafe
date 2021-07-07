8150909e7409d96b5f3ab0373626015effa18264
jessaclark <jemahold@icloud.com>
2021-07-07 10:41:40 -0400

    final commit

--- src/components/Displayall.jsx
+++ src/components/Displayall.jsx
@@ -4,13 +4,19 @@ import Mood from "./Mood"
 function Displayall(props) {
   const [mood, setMood] = useState("");
 
-
+  /* filter through poems and pull out the mood category. Search for and include all moods */
   const filteredMood = props.poems.filter(
     (poem) => poem.fields.mood.includes(mood)
   );
 
   return (
     <div className="display-all">
+        {/* drop-dowm menu with value of mood. When mood is selected give it the value of selection */}
+      <div className="browse">
+        <h2 className="browseall">Browse all.</h2>
+        <p className="sip">Browse all submitted poetry. Sort by mood. Find your new favorite poet or revisit an old one. Relax, sip and enjoy!</p>
+      </div>
+      
       <form>
           <select 
           className="down"
@@ -28,6 +34,8 @@ function Displayall(props) {
             <option value="melancholy">melancholy</option>
           </select>
           </form>
+          {/* map through moods according to mood selected and pull poems associated with that mood.
+          Render those poems to the page */}
       {filteredMood.map((poem) => (
         <div className="info" key={poem.id}>
           <Mood poem={poem} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,27 @@
-import { useParams, NavLink, } from "react-router-dom";
+import { useParams, NavLink } from "react-router-dom";
 import Mood from "./Mood";
 import { Button } from "react-bootstrap";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './Home.css'
+import "./Home.css";
 
-const Home = (props) => { 
+const Home = (props) => {
   const homeRef = useRef([0]);
   const params = useParams();
   console.log(props.poems, params);
 
-  const goToMood = () => {
-    homeRef.current.scrollIntoView({behavior: "smooth", block: "start"});
-  };
+  useEffect(() => {
+    if (params.mood) {
+      homeRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  }, [params.mood]);
 
   const filteredMood = props.poems.filter(
     (poem) => poem.fields.mood === params.mood
   );
-  
+  /* take poems and reduce them into categories and current poem. if the categories does not include the current
+  mood then create a new mood by pushing the categories to the render page and API. If it does match then send 
+  it to the current category*/
   const moods = props.poems.reduce((categories, current) => {
     if (!categories.includes(current.fields.mood)) {
       categories.push(current.fields.mood);
@@ -28,28 +32,27 @@ const Home = (props) => {
   return (
     <div className="infoContainer">
       <p className="question">What's your mood?</p>
-      <div 
-      className="buttonContainer">
-      {moods.map((mood) => (
-        <NavLink to={`/mood/${mood}`}>
-          <Button
-          onClick={goToMood}
-          variant="primary"
-          className="custom-btn"
-          align="center" 
-          type="submit" 
-          id={mood}
-          value={mood}
-          >{mood}</Button>
+      <div className="buttonContainer">
+        {moods.map((mood) => (
+          <NavLink to={`/mood/${mood}`}>
+            <Button
+              variant="primary"
+              className="custom-btn"
+              align="center"
+              type="submit"
+              id={mood}
+              value={mood}
+            >
+              {mood}
+            </Button>
           </NavLink>
-      ))}
+        ))}
       </div>
       <div ref={homeRef}>
-      {filteredMood.map((poem) => (
-        <div className="info"
-        key={poem.id}>
-          <Mood poem={poem} />
-        </div>
+        {filteredMood.map((poem) => (
+          <div className="info" key={poem.id}>
+            <Mood poem={poem} />
+          </div>
         ))}
       </div>
     </div>
