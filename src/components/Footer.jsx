4ccc7af617822f46a0dd89f7a858df756401bc41
jessaclark <jemahold@icloud.com>
2021-07-07 11:15:27 -0400

    final commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,11 @@ function Footer() {
     <div className="footer">
       <p>©2021 Jessa Clark</p>
       <a href="https://github.com/jessa-clark">
-      <img src="https://img.icons8.com/material-outlined/24/000000/github.png"/>
+      <img src="https://img.icons8.com/material-outlined/24/000000/github.png" alt="github"/>
+      
       </a>
       <a href="https://www.linkedin.com/in/jessa-clark">
-      <img src="https://img.icons8.com/material/24/000000/linkedin--v1.png"/>
+      <img src="https://img.icons8.com/material/24/000000/linkedin--v1.png" alt="linkedin"/>
       </a>
   </div>
   )
